b3d31a0d59456e0f8b891351f0d83fd1cac1ad27
Gradnos <sdolaqidze@gmail.com>
2023-03-26 18:10:35 +0400

    Add a search button and connect it to input + make search button activate on Enter key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./style.css"
+import { gradients, gradientsArr, transitionBodyBackground } from "./utility";
 import * as weatherFetcher from './weatherFetcher.js';
-import {transitionBodyBackground} from './utility';
-
 
     
     
@@ -12,19 +11,8 @@ import {transitionBodyBackground} from './utility';
 let r = document.querySelector(':root');
 let rs = getComputedStyle(r);
 
-let gradients = {
-    morningGradient : "linear-gradient(0deg, rgba(255,255,255,1) -100%, rgba(0,194,255,1) 100%)",
-    middayGradient : "linear-gradient(0deg, rgba(0,245,255,1) 0%, rgba(67,130,237,1) 100%)",
-    afternoonGradient : "linear-gradient(0deg, rgba(0,92,170,1) 0%, rgba(0,34,126,1) 100%)",
-    nightGradient : "linear-gradient(0deg, rgba(15,0,40,1) 0%, rgba(0,9,40,1) 100%)",
-    cloudylightGradient : "linear-gradient(0deg, rgba(127,139,150,1) 0%, rgba(166,179,214,1) 100%)",
-    cloudydarkGradient : "linear-gradient(0deg, rgba(41,43,43,1) 0%, rgba(84,92,135,1) 100%)",
-}
 
-
-
-let gradientsArr = Object.values(gradients);
-
+transitionBodyBackground(gradientsArr[0]);
 
 let i = 1;
 
@@ -33,6 +21,20 @@ let button = document.querySelector("button");
 button.addEventListener("click", () =>{
     transitionBodyBackground(gradientsArr[i]);
     i++;
+});
+
+let searchButton = document.querySelector(".search-button");
+let searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keypress", (e)=>{
+    if(e.key === "Enter"){
+        searchButton.click();
+    }
+});
+
+searchButton.addEventListener("click", ()=>{
+    let cityName = searchInput.value;
+    console.log(cityName);
 });
 
 
--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,12 @@
+export let gradients = {
+    morningGradient : "linear-gradient(0deg, rgba(255,255,255,1) -100%, rgba(0,194,255,1) 100%)",
+    middayGradient : "linear-gradient(0deg, rgba(0,245,255,1) 0%, rgba(67,130,237,1) 100%)",
+    afternoonGradient : "linear-gradient(0deg, rgba(0,92,170,1) 0%, rgba(0,34,126,1) 100%)",
+    nightGradient : "linear-gradient(0deg, rgba(15,0,40,1) 0%, rgba(0,9,40,1) 100%)",
+    cloudylightGradient : "linear-gradient(0deg, rgba(127,139,150,1) 0%, rgba(166,179,214,1) 100%)",
+    cloudydarkGradient : "linear-gradient(0deg, rgba(41,43,43,1) 0%, rgba(84,92,135,1) 100%)",
+}
+export let gradientsArr = Object.values(gradients);
 
 
 
